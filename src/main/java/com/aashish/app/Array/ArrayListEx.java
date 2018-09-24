d8cbd79a3d642c6ae440dfc17c23d3adfe9a1c69
aashish <aashish.jha@nascentinfo.com>
2018-09-24 18:30:20 +0530

    second java commit

---
+++ src/main/java/com/aashish/app/Array/ArrayListEx.java
@@ -0,0 +1,15 @@
+package com.aashish.app.Array;
+
+import jdk.nashorn.internal.runtime.JSONFunctions;
+import netscape.javascript.JSObject;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<Integer> intArr = new ArrayList<Integer>();
+
+    }
+
+
+}
---
+++ src/main/java/com/aashish/app/PredefinedClass.java
@@ -0,0 +1,15 @@
+package com.aashish.app;
+
+/**
+ * Create class Using Predefained Class Name Called Number.
+ */
+
+class Number {
+    public static void main(String[] args) {
+        System.out.println("Number Class Working Fine");
+    }
+}
+
+
+
+
