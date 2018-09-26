ec8201c7ece31a7f9c13fd3e38196ae70130b457
aashish <aashish.jha@nascentinfo.com>
2018-09-26 18:47:35 +0530

    fourth java commit

--- src/main/java/com/aashish/app/oops/MultiLevelInheritance.java
+++ src/main/java/com/aashish/app/oops/MultiLevelInheritance.java
@@ -6,19 +6,32 @@ import com.aashish.app.String.StringGuide;
  * Lets say there is a class called A, which extends B.
  */
 class Parent {
+
     public double age = 60;
 
     public double getAge() {
         System.out.println("Parent Age : " + this.age);
         return this.age;
     }
+
+    public Parent() {
+        System.out.println("In parent const");
+    }
 }
 
 class Child extends Parent {
 
-    public String name = "Child";
-    public int age = 30;
+    public String name;
+    public double age;
 
+    public Child() {
+        System.out.println("In child const");
+    }
+
+    Child(String name, double age) {
+        this.name = name;
+        this.age = age;
+    }
 
     @Override
     public double getAge() {
@@ -32,10 +45,20 @@ class Child extends Parent {
 }
 
 class GrandChild extends Child {
+
+
     private double age;
     private String name;
+//    Child ch = new Child("Child", 30);
+
+    public GrandChild() {
+
+        System.out.println("Ing rand child");
+
+    }
 
     public GrandChild(double age) {
+        super("s", 1);
         this.age = age;
     }
 
@@ -61,6 +84,8 @@ public class MultiLevelInheritance extends StringGuide {
     public static void main(String[] args) {
         MultiLevelInheritance M = new MultiLevelInheritance();
         M.init();
+
+        GrandChild gd = new GrandChild();
     }
 
     public void init() {
---
+++ src/main/java/com/aashish/app/oops/MultipleInheritance.java
@@ -0,0 +1,34 @@
+package com.aashish.app.oops;
+
+/**
+ * We can perform multiple inheritance in java.
+ */
+
+public class MultipleInheritance implements Tets1, Test2 {
+
+
+    public static void main(String[] args) {
+        MultipleInheritance ml = new MultipleInheritance();
+        System.out.println("Name : " + ml.getName("Aashish") + " Age : " + ml.getAge(12));
+    }
+
+    @Override
+    public String getName(String name) {
+        return name;
+    }
+
+    @Override
+    public double getAge(double age) {
+        return age;
+    }
+}
+
+interface Tets1 {
+    String getName(String name);
+
+}
+
+interface Test2 {
+    double getAge(double Age);
+}
+
